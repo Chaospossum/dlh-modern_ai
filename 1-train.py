6fa17edd578a22c67b3aa96f61b6da3577002b37
Chaospossum <duqni042@gmail.com>
2026-07-24 00:30:35 +0200

    Add initial project structure and configurations for tree-based models

---
+++ 1-train.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""function train_tree(clf, X, y) to train a tree-based classifier using Scikit-learn.
+
+Arguments:
+
+clf: A Scikit-learn classifier instance
+X: Input features
+y: Target labels
+Returns:
+
+None
+"""
+
+import from sklearn.tree import DecisionTreeClassifier
+
+def train_tree(clf, X, y):
+    clf.fit(X, y)
