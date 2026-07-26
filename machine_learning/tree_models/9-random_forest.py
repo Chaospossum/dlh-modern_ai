469d7fc6bd44dc795a4ea8a24f9859c3628dc802
Chaospossum <duqni042@gmail.com>
2026-07-26 23:24:20 +0200

    Add `get_best_alpha` function in `8-best_ccp_alpha.py` for optimal post-pruning alpha selection
    Signed-off-by: Chaospossum <duqni042@gmail.com>

--- machine_learning/tree_models/9-random_forest.py
+++ machine_learning/tree_models/9-random_forest.py
@@ -13,5 +13,6 @@ def random_forest(n_estimators, random_state):
     Returns:
     model: A Scikit-learn RandomForestClassifier instance."""
 
-    model = ensemble.RandomForestClassifier(n_estimators=20, random_state=40000)
+    model = RandomForestClassifier(n_estimators=200, random_state=0)
+{'bootstrap': True, 'ccp_alpha': 0.0, 'class_weight': None, 'criterion': 'gini', 'max_depth': None, 'max_features': 'sqrt', 'max_leaf_nodes': None, 'max_samples': None, 'min_impurity_decrease': 0.0, 'min_samples_leaf': 1, 'min_samples_split': 2, 'min_weight_fraction_leaf': 0.0, 'monotonic_cst': None, 'n_estimators': 200, 'n_jobs': None, 'oob_score': False, 'random_state': 0, 'verbose': 0, 'warm_start': False}
 return model
