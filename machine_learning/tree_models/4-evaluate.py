2bbd8c94b5150779c8136f4b66df14a08313c280
Chaospossum <duqni042@gmail.com>
2026-07-26 18:25:43 +0200

    Add `evaluate` function and template script for tree-based model evaluation
    Signed-off-by: Chaospossum <duqni042@gmail.com>

---
+++ machine_learning/tree_models/4-evaluate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+One-line summary of what this script does.
+
+Longer description if you need it: what it takes in, what it spits out.
+"""
+
+from sklearn import metrics
+
+
+def evaluate(true_labels, predicted_labels, class_names):
+    """Generate a classification report for the given true and predicted labels.
+
+       Args:
+           true_labels: Array-like of true labels.
+           predicted_labels: Array-like of predicted labels.
+           class_names: List of class names (e.g., ['class_0', 'class_1']).
+
+       Returns:
+           str: Classification report as a string.
+       """
+    report= metrics.classification_report(true_labels,predicted_labels,target_names=class_names,output_dict=False)
+    return report
+
