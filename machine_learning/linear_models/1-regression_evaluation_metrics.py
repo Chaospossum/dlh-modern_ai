89e31df7846461d666ca87f0db5ad7cdc6fc903d
Chaospossum <duqni042@gmail.com>
2026-08-18 01:30:16 +0200

    Add RMSE to `evaluation_metrics_for_regression` in `1-regression_evaluation_metrics.py`
    Signed-off-by: Chaospossum <duqni042@gmail.com>

--- machine_learning/linear_models/1-regression_evaluation_metrics.py
+++ machine_learning/linear_models/1-regression_evaluation_metrics.py
@@ -25,4 +25,4 @@ mae = mean_absolute_error(y_true, y_pred)
 mse = mean_squared_error(y_true, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_true, y_pred)
-return mae, mse, r2
+return mae, mse, r2 , rmse
